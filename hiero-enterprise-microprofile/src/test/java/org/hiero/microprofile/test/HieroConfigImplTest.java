a961f94ee079bd41f7ac957eadc7fa0eda504351
Aman <amkr6207@gmail.com>
2026-04-27 21:52:09 +0530

    test(microprofile): add custom network empty chainId/relayUrl coverage
    Signed-off-by: Aman <amkr6207@gmail.com>

--- hiero-enterprise-microprofile/src/test/java/org/hiero/microprofile/test/HieroConfigImplTest.java
+++ hiero-enterprise-microprofile/src/test/java/org/hiero/microprofile/test/HieroConfigImplTest.java
@@ -56,4 +56,48 @@ class HieroConfigImplTest {
     assertEquals(Optional.of(296L), config.chainId());
     assertEquals(Optional.of("https://testnet.hashio.io/api"), config.relayUrl());
   }
+
+  @Test
+  void shouldReturnEmptyChainIdAndRelayUrlForCustomNetwork() {
+    final HieroOperatorConfiguration operatorConfiguration =
+        new HieroOperatorConfiguration() {
+          @Override
+          public String getAccountId() {
+            return "0.0.1001";
+          }
+
+          @Override
+          public String getPrivateKey() {
+            return PrivateKey.generateED25519().toString();
+          }
+        };
+
+    final HieroNetworkConfiguration networkConfiguration =
+        new HieroNetworkConfiguration() {
+          @Override
+          public Optional<String> getName() {
+            return Optional.of("custom-network");
+          }
+
+          @Override
+          public Optional<String> getMirrornode() {
+            return Optional.empty();
+          }
+
+          @Override
+          public Optional<Long> getRequestTimeoutInMs() {
+            return Optional.empty();
+          }
+
+          @Override
+          public Set<ConsensusNode> getNodes() {
+            return Set.of();
+          }
+        };
+
+    final HieroConfigImpl config = new HieroConfigImpl(operatorConfiguration, networkConfiguration);
+
+    assertEquals(Optional.empty(), config.chainId());
+    assertEquals(Optional.empty(), config.relayUrl());
+  }
 }
