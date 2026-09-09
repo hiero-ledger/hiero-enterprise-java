0efee80dfc7ec3bf6163d523032df2f2dbc8d28a
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2026-09-10 01:48:31 +0300

    feat: Support airdropping NFTs to multiple receivers via map overload
    Signed-off-by: Ndacyayisenga-droid <ndacyayinoah@gmail.com>

--- hiero-enterprise-base/src/test/java/org/hiero/base/test/NftClientImplTest.java
+++ hiero-enterprise-base/src/test/java/org/hiero/base/test/NftClientImplTest.java
@@ -861,6 +861,7 @@ public class NftClientImplTest {
     final PrivateKey privateKey = PrivateKey.generateECDSA();
     final PublicKey publicKey = privateKey.getPublicKey();
 
+
     final TokenId tokenId = TokenId.fromString("1.2.3");
     final Account account = new Account(accountId, publicKey, privateKey);
 
